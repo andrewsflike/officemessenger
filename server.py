261e494ca2a72ba4de07a2ce58d3b63260433c93
Andrew <andrewsflike@gmail.com>
2026-02-05 10:50:42 -0500

    Refactor server.py to define port variable

--- server.py
+++ server.py
@@ -96,4 +96,7 @@ def handle_message(data):
 
 if __name__ == '__main__':
     init_db()
-    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port, debug=False)
+
